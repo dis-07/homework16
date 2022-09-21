d28ec25c1ff7e086804db517d39521eefdbbcefd
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2022-09-21 17:01:24 +0300

    fixed function

--- js/script.js
+++ js/script.js
@@ -1,37 +1,43 @@
 const API_URL = 'https://jsonplaceholder.typicode.com/albums';
 
 const getAlboms = async () => {
-    let ul = document.createElement('ul');
-    ul.className = 'albums';
-    document.body.append(ul);
-
-    ul.addEventListener('click', (event) => {
-        const isRemoveButton = event.target.className === 'remove-btn';
-
-        if (isRemoveButton) {
-            const removeButton = event.target;
-            const albumsTitle = removeButton.closest('.album_title');
-            albumsTitle.remove();
-        }
-    })
     try {
         const responce = await fetch (API_URL);
-        const data = await responce.json();
-        data.forEach(item => {
-
-            let li = document.createElement('li');
-            li.className = 'album_title';
-            li.innerHTML = item.title;
-            ul.append(li);
-
-            let button = document.createElement('button');
-            button.innerHTML = 'remove';
-            button.className = 'remove-btn';
-            li.append(button);
-        });
+        let data = await responce.json();
+        return data;
     }   catch (err) {
         console.error(err);
     }
 }
 
-getAlboms();
+let ul = document.createElement('ul');
+ul.className = 'albums';
+document.body.append(ul);
+
+const createAlbums = async () => {
+    let title = await getAlboms();
+
+    title.forEach(item => {
+        let li = document.createElement('li');
+        li.className = 'album_title';
+        li.innerHTML = item.title;
+        ul.append(li);
+
+        let button = document.createElement('button');
+        button.className = 'remove-btn';
+        li.append(button);
+        button.innerHTML = 'remove';
+    })
+}
+
+ul.addEventListener('click', (event) => {
+    const isRemoveButton = event.target.className === 'remove-btn';
+
+    if (isRemoveButton) {
+        const removeButton = event.target;
+        const albumsTitle = removeButton.closest('.album_title');
+        albumsTitle.remove();
+    }
+})
+
+createAlbums();
